116cfaab3fa5c2c4b1c9183b7f29b0dc05c59d58
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-05-31 14:17:53 -0400

    creating env

--- clirp.py
+++ clirp.py
@@ -5,16 +5,17 @@ import os
 print('Welcome to CLIRP: An rpn calculator for your terminal!')
 
 def main(win):
+    string = ''
     win.nodelay(True)
     key=''
     win.clear()
-    win.addstr('detected key')
+    win.addstr('Welcome to CLIRP: An rpm calculator for your terminal!')
     while 1:
         try:
             key = win.getkey()
             win.clear()
-            win.addstr('detected key')
-            win.addstr(str(key))
+            win.addstr(string)
+            string += key
             if key == os.linesep:
                 break
         except Exception as e:
