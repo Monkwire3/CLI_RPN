35182d90afec45e92a4a2d00f3d1d8ea6d19b46d
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-06-12 15:29:22 -0400

    set rich as default

--- clirp.py
+++ clirp.py
@@ -1,57 +1,38 @@
-import curses 
-import os
-
-print('Welcome to CLIRP: An rpn calculator for your terminal!')
-def buildOverlay(win):
-    win.nodelay(True)
-    # win.clear()
-    win.addstr('OVERLAY')
-    win.addstr('=' * 100)
-
-def addPad():
-    pad = curses.newpad(100, 100)
-    # These loops fill the pad with letters; addch() is
-    # explained in the next section
-    for y in range(0, 99):
-        for x in range(0, 99):
-            pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
-
-    # Displays a section of the pad in the middle of the screen.
-    # (0,0) : coordinate of upper-left corner of pad area to display.
-    # (5,5) : coordinate of upper-left corner of window area to be filled
-    #         with pad content.
-    # (20, 75) : coordinate of lower-right corner of window area to be
-    #          : filled with pad content.
-    pad.refresh( 0,0, 5,5, 20,75)
+from rich import print
+from rich.panel import Panel
+from rich.layout import Layout
+from rich.live import Live
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.console import Capture
+import keyboard
+import time
 
 
-def renderHeader(win):
-    # r, c = win.getmaxyx()
-    # win.addstr(0, 0, f"{'=' * ((c // 2) - 6)} <HEADER> {'=' * ((c // 2) - 6)}") 
+def main():
+    console = Console()
+    _width, _height = console.width, console.height
+    console.size = (_width, _height-1)
+    
+    layout = Layout(size=console.height)
+    layout.split_column(
+        Layout(name="upper"),
+        Layout(name="lower")
+    )
+    layout["upper"].update(
+        Panel("[red]CLIRP")
+    )
+    layout["upper"].ratio = 1
+    layout["lower"].ratio = 5
 
-    win.border('ls', 'rs', 'ts', 'bs', 'tl', 'bl', 'br')
+    
 
-
-
-def main(win):
-    string = ''
-    win.nodelay(True)
-    key=''
-    win.clear()
-    win.addstr(0, 0, 'Welcome to CLIRP: An rpn calculator for your terminal!\n')
-    win.addstr('Press any key to start.')
     while True:
-        try:
-            key = win.getkey()
-            if win.getch() == curses.KEY_BACKSPACE:
-                string += 'BACKSPACE!!!!'
-            string += key
-            # string += key if len(key) == 1 else ''
-            win.clear()
-            win.addstr(10, 10, string)
-            renderHeader(win)
-        except Exception as e:
-            pass
+        name = Prompt.ask("> ")
+        layout["lower"].update(Panel(f"[blue]{name}"))
+        console.log(layout)
 
-curses.wrapper(main)
-        
+                
+
+
+main()
